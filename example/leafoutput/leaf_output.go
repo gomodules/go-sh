c7a7b864ed781567d21e22b5288b170fc9c565ed
Anisur Rahman <54911684+anisurrahman75@users.noreply.github.com>
2026-06-05 12:38:09 +0600

    Add current output buffer feature for leaf Command (#6)
    Signed-off-by: Anisur Rahman <anisur@appscode.com>

---
+++ example/leafoutput/leaf_output.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"gomodules.xyz/go-sh"
+)
+
+func main() {
+	s := sh.NewSession()
+	s.ShowCMD = true
+	s.Command("seq", "1", "20000000").LeafCommand("xargs")
+	//s.Command("seq", "1", "2000000000")
+	var out []byte
+	var err error
+
+	ch := make(chan struct{})
+	go func() {
+		out, err = s.Output()
+		//fmt.Println("out:", string(out))
+		ch <- struct{}{}
+	}()
+
+	tick := time.NewTicker(1 * time.Second)
+	defer tick.Stop()
+loop:
+	for {
+		select {
+		case <-tick.C:
+			fmt.Println("tick")
+			out0, _ := s.CurrentOutput(0)
+			size := len(out0)
+			fmt.Println("out0 size:", size)
+			//fmt.Println("out0:", string(out0[size-100:]))
+		case <-ch:
+			break loop
+		}
+	}
+
+	fmt.Println("done")
+
+	fmt.Println("Output:", len(string(out)))
+	fmt.Println("Error:", err)
+
+}
